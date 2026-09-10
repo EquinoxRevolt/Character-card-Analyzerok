8620b8b193be83c53d1f0a31b1320586b8357535
EquinoxRevolt <104873265+EquinoxRevolt@users.noreply.github.com>
2026-09-10 16:25:37 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // CRITICAL: This must exactly match your repository name!
   base: '/Character-card-Analyzerok/',
   plugins: [
     react(),
